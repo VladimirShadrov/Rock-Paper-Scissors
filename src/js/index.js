64cfe524e4c248e6d928c2648b5f3231fecc96fe
Vladimir <shadrov_vladimir@mail.ru>
2021-05-01 10:10:09 +0300

    Добавлена верстка экрана step-1

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import '../styles/styles.scss';
+import '../styles/header.scss';
+import '../styles/step-1.scss';
+// import '../styles/step-2.scss';
+// import '../styles/step-3.scss';
+import '../styles/rules.scss';
+
+// Перенос изображений
+require.context('../images', true, /\.(png|jpg|svg|gif)$/);
